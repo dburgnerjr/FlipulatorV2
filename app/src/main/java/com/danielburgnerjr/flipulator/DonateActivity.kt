444b5069e8760267e4957497101888331452727e
Daniel Burgner <dburgnerjr@yahoo.com>
2019-05-27 00:08:02 -0500

    D56 DonateActivity Part 3
    D56 of #100DaysOfCode #2019 challenge.  Started rebuilding DonateActivity from the ground up for #FlipulatorV2 app.  #UnfinishedBusiness #AndroidDev

--- app/src/main/java/com/danielburgnerjr/flipulator/DonateActivity.kt
+++ app/src/main/java/com/danielburgnerjr/flipulator/DonateActivity.kt
@@ -1,223 +1,77 @@
 package com.danielburgnerjr.flipulator
 
-import android.app.Activity
-import android.app.AlertDialog
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-class DonateActivity : Activity() {
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
 
-    private var mGoogleSpinner: Spinner? = null
-    private var btnDonateNow: Button? = null                // donate google
-    private var btnPayPal: Button? = null                    // donate paypal
-    private var txtPayPalTitle: TextView? = null            // Paypal title
-    private var txtPayPalDesc: TextView? = null                // Paypal description
+import java.util.ArrayList;
+import java.util.List;
 
-    // Google Play helper object
-    private var mHelper: IabHelper? = null
+class DonateActivity : AppCompatActivity() {
 
-    protected var mDebug = false
+    var mBuyButton: Button? = null
+    var mAdRemovalPrice: String? = null
+    var mSharedPreferences: SharedPreferences? = null
 
-    protected var mGoogleEnabled = false
-    protected var mGooglePubkey = ""
-    protected var mGoogleCatalog = arrayOf<String>()
-    protected var mGoogleCatalogValues = arrayOf<String>()
+    lateinit private var mBillingClient: BillingClient
 
-    protected var mPaypalEnabled = false
-    protected var mPaypalUser = ""
-    protected var mPaypalCurrencyCode = ""
-    protected var mPaypalItemName = ""
-
-    // Callback for when a purchase is finished
-    internal var mPurchaseFinishedListener: IabHelper.OnIabPurchaseFinishedListener = object : IabHelper.OnIabPurchaseFinishedListener {
-        override fun onIabPurchaseFinished(result: IabResult?, purchase: Purchase?) {
-            if (mDebug)
-                Log.d(TAG, "Purchase finished: $result, purchase: $purchase")
-
-            // if we were disposed of in the meantime, quit.
-            if (mHelper == null) return
-
-            if (result.isSuccess) {
-                if (mDebug)
-                    Log.d(TAG, "Purchase successful.")
-
-                // directly consume in-app purchase, so that people can donate multiple times
-                mHelper!!.consumeAsync(purchase, mConsumeFinishedListener)
-
-                // show thanks openDialog
-                openDialog(android.R.drawable.ic_dialog_info, R.string.donations__thanks_dialog_title,
-                        getString(R.string.donations__thanks_dialog))
-            }
-        }
-    }
-
-    // Called when consumption is complete
-    internal var mConsumeFinishedListener: IabHelper.OnConsumeFinishedListener = object : IabHelper.OnConsumeFinishedListener {
-        override fun onConsumeFinished(purchase: Purchase, result: IabResult) {
-            if (mDebug)
-                Log.d(TAG, "Consumption finished. Purchase: $purchase, result: $result")
-
-            // if we were disposed of in the meantime, quit.
-            if (mHelper == null) return
-
-            if (result.isSuccess) {
-                if (mDebug)
-                    Log.d(TAG, "Consumption successful. Provisioning.")
-            }
-            if (mDebug)
-                Log.d(TAG, "End consumption flow.")
-        }
-    }
-
-    public override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.donate_activity)
 
-        mGooglePubkey = GOOGLE_PUBKEY
-        mGoogleCatalog = GOOGLE_CATALOG
-        mGoogleCatalogValues = resources.getStringArray(R.array.donation_google_catalog_values)
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 
-        txtPayPalTitle = findViewById(R.id.txtPaypalTitle) as TextView
-        txtPayPalDesc = findViewById(R.id.txtPaypalDescription) as TextView
+        // Establish connection to billing client
+        mBillingClient = BillingClient.newBuilder(this@DonateActivity).setListener(this).build()
+        mBillingClient.startConnection(object : BillingClientStateListener {
+            fun onBillingSetupFinished(@BillingClient.BillingResponse billingResponseCode: Int) {
+                if (billingResponseCode == BillingClient.BillingResponse.OK) {
+                    // The billing client is ready. You can query purchases here.
 
-        // choose donation amount
-        mGoogleSpinner = findViewById(R.id.spnDonate) as Spinner
-        val adapter: ArrayAdapter<CharSequence>
-        if (mDebug) {
-            adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, CATALOG_DEBUG)
-        } else {
-            adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, mGoogleCatalogValues)
-        }
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        mGoogleSpinner!!.adapter = adapter
-        btnDonateNow = findViewById(R.id.btnDonateNow) as Button
-
-        btnDonateNow!!.setOnClickListener { v -> donateGoogleOnClick(v) }
-
-        // Create the helper, passing it our context and the public key to verify signatures with
-        if (mDebug)
-            Log.d(TAG, "Creating IAB helper.")
-        mHelper = IabHelper(this, mGooglePubkey)
-
-        // enable debug logging (for a production application, you should set this to false).
-        mHelper!!.enableDebugLogging(mDebug)
-
-        // Start setup. This is asynchronous and the specified listener
-        // will be called once setup completes.
-        if (mDebug)
-            Log.d(TAG, "Starting setup.")
-        mHelper!!.startSetup(object : IabHelper.OnIabSetupFinishedListener {
-            override fun onIabSetupFinished(result: IabResult) {
-                if (mDebug)
-                    Log.d(TAG, "Setup finished.")
-
-                if (!result.isSuccess) {
-                    // Oh noes, there was a problem.
-                    openDialog(android.R.drawable.ic_dialog_alert, R.string.donations__google_android_market_not_supported_title,
-                            getString(R.string.donations__google_android_market_not_supported))
-                    return
                 }
+            }
 
-                // Have we been disposed of in the meantime? If so, quit.
-                if (mHelper == null) return
+            override fun onBillingServiceDisconnected() {
+                //TODO implement your own retry policy
+                Toast.makeText(this@DonateActivity, resources.getString(R.string.billing_connection_failure), Toast.LENGTH_SHORT)
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
             }
         })
 
-        btnPayPal = findViewById(R.id.btnDonatePaypal) as Button
 
-        // set PayPal invisible for Google Play
-        txtPayPalTitle!!.visibility = View.INVISIBLE
-        txtPayPalDesc!!.visibility = View.INVISIBLE
-        btnPayPal!!.visibility = View.INVISIBLE
+        mBuyButton = findViewById<View>(R.id.buyButton) as Button
 
-        btnPayPal!!.setOnClickListener { v -> donatePayPalOnClick(v) }
+        mBuyButton.setOnClickListener(View.OnClickListener {
+            // If user clicks the buy button, launch the billing flow for an ad removal  purchase
+            // Response is handled using onPurchasesUpdated listener
+            val flowParams = BillingFlowParams.newBuilder()
+                    .setSku(ITEM_SKU_ADREMOVAL)
+                    .setType(BillingClient.SkuType.INAPP)
+                    .build()
+            val responseCode = mBillingClient.launchBillingFlow(this@DonateActivity, flowParams)
+        })
 
+        // Query purchases incase a user is connecting from a different device and they've already purchased the app
+        queryPurchases()
+        queryPrefPurchases()
     }
 
-    /**
-     * Open dialog
-     */
-    internal fun openDialog(icon: Int, title: Int, message: String) {
-        val dialog = AlertDialog.Builder(this)
-        dialog.setIcon(icon)
-        dialog.setTitle(title)
-        dialog.setMessage(message)
-        dialog.setCancelable(true)
-        dialog.setNeutralButton(R.string.donations__button_close
-        ) { dialog, which -> dialog.dismiss() }
-        dialog.show()
-    }
-
-    /**
-     * Donate button executes donations based on selection in spinner
-     */
-    fun donateGoogleOnClick(view: View) {
-        val index: Int
-        index = mGoogleSpinner!!.selectedItemPosition
-        if (mDebug)
-            Log.d(TAG, "selected item in spinner: $index")
-
-        if (mDebug) {
-            // when debugging, choose android.test.x item
-            //Toast.makeText(getApplicationContext(), CATALOG_DEBUG[index], Toast.LENGTH_LONG).show();
-            mHelper!!.launchPurchaseFlow(this,
-                    CATALOG_DEBUG[index], IabHelper.ITEM_TYPE_INAPP,
-                    0, mPurchaseFinishedListener, null)
-        } else {
-            //Toast.makeText(getApplicationContext(), mGoogleCatalog[index], Toast.LENGTH_LONG).show();
-            mHelper!!.launchPurchaseFlow(this,
-                    mGoogleCatalog[index], IabHelper.ITEM_TYPE_INAPP,
-                    0, mPurchaseFinishedListener, null)
-        }
-    }
-
-    /**
-     * Donate Paypal button executes link to Paypal donation page
-     */
-    fun donatePayPalOnClick(view: View) {
-        val strPaypal = "http://www.paypal.me/dburgnerjr"
-        val newActivity = Intent(Intent.ACTION_VIEW, Uri.parse(strPaypal))
-        startActivity(newActivity)
-
-    }
-
-    /**
-     * Needed for Google Play In-app Billing. It uses startIntentSenderForResult(). The result is not propagated to
-     * the Fragment like in startActivityForResult(). Thus we need to propagate manually to our Fragment.
-     */
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
-        if (mDebug)
-            Log.d(TAG, "onActivityResult($requestCode,$resultCode,$data")
-        if (mHelper == null) return
-
-        // Pass on the fragment result to the helper for handling
-        if (!mHelper!!.handleActivityResult(requestCode, resultCode, data)) {
-            // not handled, so handle it ourselves (here's where you'd
-            // perform any handling of activity results not related to in-app
-            // billing...
-            super.onActivityResult(requestCode, resultCode, data)
-        } else {
-            if (mDebug)
-                Log.d(TAG, "onActivityResult handled by IABUtil.")
-        }
-    }
-
-    companion object {
-
-        private val TAG = "Donations Library"
-
-        private val CATALOG_DEBUG = arrayOf("android.test.purchased", "android.test.canceled", "android.test.refunded", "android.test.item_unavailable")
-        private val GOOGLE_CATALOG = arrayOf("donation1", "donation5", "donation10", "donation25", "donation50", "donation100")
-        /**
-         * Google
-         */
-        private val GOOGLE_PUBKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhUoMs4c4RcEIRckCir+wNCDZk8Zy3I4w7UjrPvKWIh8tl71HrEE+6BQiBCxseGfpuVEZntaHhzyQj5gMLSI5JBRboOlpItj7SyvupoHszsSh28VdJQiD3AWXB1LNeS9Z6W9RffSKYfEKs8v+dMwqzi+C2M+fPg9o7IcAXsCnJrVqS+vIhYrfyVX4oG3DQ28wfcWVPgGnNLP82y5VaP+xlJYdBZBQJrDBlQq1QaecSiR+wRG8ZBMv5V1x6w/QM4yKhoolz2Pc6Zt8YVkCVpQhf8usGcAy0d6ysW5YlkhFz2PbVoi553OkH3T5lZ5LO3cFXYG0g1ttsfE/8WiPYRlGxwIDAQAB"
-    }
 }
