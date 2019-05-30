1e988c112d4237484ed8d7fda9691f5ece7464c8
Daniel Burgner <dburgnerjr@yahoo.com>
2019-05-29 21:38:57 -0500

    D59 Revamped DonateActivity Part 6
    D59 of #100DaysOfCode #2019 challenge.  Reworked DonateActivity to a simplified version for #FlipulatorV2 app.  #UnfinishedBusiness #AndroidDev

--- app/src/main/java/com/danielburgnerjr/flipulator/DonateActivity.kt
+++ app/src/main/java/com/danielburgnerjr/flipulator/DonateActivity.kt
@@ -1,48 +1,38 @@
 package com.danielburgnerjr.flipulator
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.annotation.Nullable
+import android.support.v7.app.AppCompatActivity
 import android.transition.Visibility
-import android.util.Log;
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View;
+import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 
-import com.android.billingclient.api.BillingClient;
-import com.android.billingclient.api.BillingClient.BillingResponseCode
-import com.android.billingclient.api.BillingClientStateListener;
-import com.android.billingclient.api.BillingFlowParams;
-import com.android.billingclient.api.Purchase;
-import com.android.billingclient.api.PurchasesUpdatedListener;
-import com.android.billingclient.api.SkuDetails;
-import com.android.billingclient.api.SkuDetailsParams;
-import com.android.billingclient.api.SkuDetailsResponseListener;
+import com.android.billingclient.api.*
+import kotlinx.android.synthetic.main.donate_activity.*
 
 class DonateActivity : AppCompatActivity(), PurchasesUpdatedListener {
 
-    private val TAG = "InAppBilling"
-
-    var mSharedPreferences: SharedPreferences? = null
-    private val CATALOG_DEBUG = arrayOf("android.test.purchased", "android.test.canceled", "android.test.refunded", "android.test.item_unavailable")
-    private val GOOGLE_CATALOG = arrayOf("donation1", "donation5", "donation10", "donation25", "donation50", "donation100")
-
-    lateinit private var mBillingClient: BillingClient
+    private lateinit var mBillingClient: BillingClient
+    private lateinit var productAdapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.donate_activity)
+        setupBillingClient()
 
+/*
         val mGoogleSpinner: Spinner = findViewById(R.id.spnDonate)
         val strOptions = resources.getStringArray(R.array.donation_google_catalog_values)
         var strSelectedOption: String = ""
@@ -62,26 +52,6 @@ class DonateActivity : AppCompatActivity(), PurchasesUpdatedListener {
                 // TODO Auto-generated method stub
             }
         }
-
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-
-        // Establish connection to billing client
-        mBillingClient = BillingClient.newBuilder(this@DonateActivity).setListener(this).build()
-        mBillingClient.startConnection(object : BillingClientStateListener {
-            fun onBillingSetupFinished(@BillingClient.BillingResponseCode billingResponseCode: Int) {
-                if (billingResponseCode == BillingClient.BillingResponseCode.OK) {
-                    // The billing client is ready. You can query purchases here.
-
-                }
-            }
-
-            override fun onBillingServiceDisconnected() {
-                //TODO implement your own retry policy
-                //Toast.makeText(this@DonateActivity, resources.getString(R.string.billing_connection_failure), Toast.LENGTH_SHORT)
-                // Try to restart the connection on the next request to
-                // Google Play by calling the startConnection() method.
-            }
-        })
 
         val btnDonateNow: Button = findViewById(R.id.btnDonateNow)
         btnDonateNow.setOnClickListener(object: View.OnClickListener {
@@ -110,46 +80,95 @@ class DonateActivity : AppCompatActivity(), PurchasesUpdatedListener {
         txtPayPalTitle.visibility = View.GONE
         txtPayPalDescription.visibility = View.GONE
         btnPayPal.visibility = View.GONE
-
-/*
-
-
-        mBuyButton = findViewById<View>(R.id.buyButton) as Button
-
-        mBuyButton.setOnClickListener(View.OnClickListener {
-            // If user clicks the buy button, launch the billing flow for an ad removal  purchase
-            // Response is handled using onPurchasesUpdated listener
-            val flowParams = BillingFlowParams.newBuilder()
-                    .setSku(ITEM_SKU_ADREMOVAL)
-                    .setType(BillingClient.SkuType.INAPP)
-                    .build()
-            val responseCode = mBillingClient.launchBillingFlow(this@DonateActivity, flowParams)
-        })
-
-        // Query purchases incase a user is connecting from a different device and they've already purchased the app
-        queryPurchases()
-        queryPrefPurchases()
 */
+
     }
 
-    fun onPurchasesUpdated(responseCode: Int, @Nullable List<com.android.billingclient.api.Purchase> purchases) {
+    private fun setupBillingClient() {
+        mBillingClient = BillingClient
+                .newBuilder(this)
+                .setListener(this)
+                .build()
 
-        //Handle the responseCode for the purchase
-        //If response code is OK,  handle the purchase
-        //If user already owns the item, then indicate in the shared prefs that item is owned
-        //If cancelled/other code, log the error
-
-        if (responseCode == BillingClient.BillingResponseCode.OK && purchases != null) {
-            for (Purchase purchase : purchases) {
-                handlePurchase(purchase);
+        mBillingClient.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(@BillingClient.BillingResponse billingResponseCode: Int) {
+                if (billingResponseCode == BillingClient.BillingResponse.OK) {
+                    println("BILLING | startConnection | RESULT OK")
+                } else {
+                    println("BILLING | startConnection | RESULT: $billingResponseCode")
+                }
             }
-        } else if (responseCode == BillingClient.BillingResponseCode.USER_CANCELED) {
-            // Handle an error caused by a user cancelling the purchase flow.
-            Log.d(TAG, "User Canceled" + responseCode);
-        } else {
-            Log.d(TAG, "Other code" + responseCode);
-            // Handle any other error codes.
-        }
 
+            override fun onBillingServiceDisconnected() {
+                println("BILLING | onBillingServiceDisconnected | DISCONNECTED")
+            }
+        })
+    }
+
+    fun onLoadProductsClicked(view: View) {
+        if (mBillingClient.isReady) {
+            val params = SkuDetailsParams
+                    .newBuilder()
+                    .setSkusList(GOOGLE_CATALOG)
+                    .setType(BillingClient.SkuType.INAPP)
+                    .build()
+            mBillingClient.querySkuDetailsAsync(params) { responseCode, skuDetailsList ->
+                if (responseCode == BillingClient.BillingResponse.OK) {
+                    println("querySkuDetailsAsync, responseCode: $responseCode")
+                    initProductAdapter(skuDetailsList)
+                } else {
+                    println("Can't querySkuDetailsAsync, responseCode: $responseCode")
+                }
+            }
+        } else {
+            println("Billing Client not ready")
+        }
+    }
+
+    private fun initProductAdapter(skuDetailsList: List<SkuDetails>) {
+        productAdapter = ProductAdapter(skuDetailsList) {
+            val billingFlowParams = BillingFlowParams
+                    .newBuilder()
+                    .setSkuDetails(it)
+                    .build()
+            mBillingClient.launchBillingFlow(this, billingFlowParams)
+        }
+        products.adapter = productAdapter
+    }
+
+    override fun onPurchasesUpdated(responseCode: Int, purchases: MutableList<Purchase>?) {
+        println("onPurchasesUpdated: $responseCode")
+        allowMultiplePurchases(purchases)
+    }
+
+    private fun allowMultiplePurchases(purchases: MutableList<Purchase>?) {
+        val purchase = purchases?.first()
+        if (purchase != null) {
+            mBillingClient.consumeAsync(purchase.purchaseToken) { responseCode, purchaseToken ->
+                if (responseCode == BillingClient.BillingResponse.OK && purchaseToken != null) {
+                    println("AllowMultiplePurchases success, responseCode: $responseCode")
+                } else {
+                    println("Can't allowMultiplePurchases, responseCode: $responseCode")
+                }
+            }
+        }
+    }
+
+    private fun clearHistory() {
+        mBillingClient.queryPurchases(BillingClient.SkuType.INAPP).purchasesList
+                .forEach {
+                    mBillingClient.consumeAsync(it.purchaseToken) { responseCode, purchaseToken ->
+                        if (responseCode == BillingClient.BillingResponse.OK && purchaseToken != null) {
+                            println("onPurchases Updated consumeAsync, purchases token removed: $purchaseToken")
+                        } else {
+                            println("onPurchases some troubles happened: $responseCode")
+                        }
+                    }
+                }
+    }
+
+    companion object {
+        private val CATALOG_DEBUG = listOf("android.test.purchased", "android.test.canceled", "android.test.refunded", "android.test.item_unavailable")
+        private val GOOGLE_CATALOG = listOf("donation1", "donation5", "donation10", "donation25", "donation50", "donation100")
     }
 }
